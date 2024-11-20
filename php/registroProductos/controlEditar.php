0c72d273bdd4594b91fbfe395859d3077904979c
Nazareno lopez <alanbolso27@gmail.com>
2024-11-20 19:26:19 -0300

    Actualizacion
    Se arreglo el problema de la edición de los campos de los productos

--- php/registroProductos/controlEditar.php
+++ php/registroProductos/controlEditar.php
@@ -1,24 +1,31 @@
 <?php 
 include "../conexion_be.php";
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $CodigoBarras = $_POST["CodigoB"];
-        $CodigoB = $_POST["CodigoB"];
-        $Precio = $_POST["Precio"];
-        $Descripcion = $_POST["Descripcion"];
-        $Stock = $_POST["Stock"];
 
-    if(!preg_match("/^[0-9]{1,13}+$/", $CodigoB) ||
-     !preg_match("/^[0-9]+$/", $Precio) ||
-     !preg_match("/^[a-zA-Z\s]+$/", $Descripcion) || 
-     !preg_match("/^[0-9]+$/", $Stock)){
-        echo '<div class="alert alert-danger">Error: Datos Incorrectos</div>';
-    }else{
-        $sql = $conexion->query("UPDATE producto SET Precio='$Precio', Descripcion='$Descripcion', Stock='$Stock' WHERE CodigoBarras='$CodigoBarras'");
-        if($sql){
-            echo "<div class='alert alert-success'>Producto modificado exitosamente.</div>";
-        }else{
-        echo "<div class='alert alert-danger'>Error al modificar</div>";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $CodigoBarras = $_POST["CodigoB"];
+    $campos = [];
+
+    // Verificar y validar los campos enviados
+    if (!empty($_POST["Precio"]) && preg_match("/^[0-9]+$/", $_POST["Precio"])) {
+        $campos[] = "Precio='{$_POST["Precio"]}'";
     }
+    if (!empty($_POST["Descripcion"]) && preg_match("/^[a-zA-Z\s]+$/", $_POST["Descripcion"])) {
+        $campos[] = "Descripcion='{$_POST["Descripcion"]}'";
+    }
+    if (!empty($_POST["Stock"]) && preg_match("/^[0-9]+$/", $_POST["Stock"])) {
+        $campos[] = "Stock='{$_POST["Stock"]}'";
+    }
+    // Verificar si hay campos válidos para actualizar
+    if (count($campos) > 0) {
+        $sql = $conexion->query("UPDATE producto SET " . implode(", ", $campos) . " WHERE CodigoBarras='$CodigoBarras'");
+
+        if ($sql) {
+            echo "<div class='alert alert-success'>Producto modificado exitosamente.</div>";
+        } else {
+            echo "<div class='alert alert-danger'>Error al modificar</div>";
+        }
+    } else {
+        echo '<div class="alert alert-danger">Error: Datos Incorrectos</div>';
     }
 }
 
